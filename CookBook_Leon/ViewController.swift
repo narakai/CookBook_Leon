e8fad07337ade070da84a0e4c23702c314688508
laileon <leon@igola.com>
2017-12-21 15:41:04 +0800

    Initial commit

---
+++ CookBook_Leon/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CookBook_Leon
+//
+//  Created by lai leon on 2017/12/21.
+//  Copyright © 2017 clem. All rights reserved.
+//
+
+import UIKit
+
+//后面的菜系菜单
+
+let YHRect = UIScreen.main.bounds
+let YHHeight = YHRect.size.height
+let YHWidth = YHRect.size.width
+let YHNoNavRect = CGRect(x: 0, y: 0, width: YHWidth, height: YHHeight - 64)
+let YHNoTarRect = CGRect(x: 0, y: 0, width: YHWidth, height: YHHeight - 49)
+let YHNoNavTarRect = CGRect(x: 0, y: 0, width: YHWidth, height: YHHeight - 49 - 64)
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CookBook_Leon' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CookBook_Leon
+  pod 'SideMenu'
+  pod 'Alamofire', '~> 4.5'
+  pod 'PKHUD', '~> 5.0'
+
+end
